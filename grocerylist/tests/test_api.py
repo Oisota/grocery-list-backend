91170a04d9660331558dfc9ffe0a0990c1f8af01
Derek Morey <dman6505@gmail.com>
2017-09-19 19:26:02 -0400

    created test scaffolding

--- grocerylist/tests/test_api.py
+++ grocerylist/tests/test_api.py
@@ -6,12 +6,28 @@ from .. import app
 from ..database import init_db
 
 class APITestCase(unittest.TestCase):
+
     def setUp(self):
-        self.db_fd, flaskr.app.config['db'] = tempfile.mkstemp()
+        self.db_fd, app.config['DB'] = tempfile.mkstemp()
         app.testing = True
         self.app = app.test_client()
-        with app.app_context():
-            init_db()
+        init_db()
 
     def tearDown(self):
-        pass
+        os.close(self.db_fd)
+        os.unlink(app.config['DB'])
+
+    def test_grocery_list_get(self):
+        self.assertTrue(False)
+
+    def test_grocery_list_post(self):
+        self.assertTrue(False)
+
+    def test_grocery_item_get(self):
+        self.assertTrue(False)
+
+    def test_grocery_item_put(self):
+        self.assertTrue(False)
+
+    def test_grocery_item_delete(self):
+        self.assertTrue(False)
--- makefile
+++ makefile
@@ -3,3 +3,7 @@ run:
 	export FLASK_APP=grocerylist && \
 	export FLASK_DEBUG=true && \
 	flask run --host=0.0.0.0 --port=8080
+
+.PHONY: test
+test:
+	python -m unittest
