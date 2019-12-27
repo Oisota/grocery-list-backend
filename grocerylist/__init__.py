da0494470863ecce423511196cbba3b634454226
Derek Morey <derek.o.morey@gmail.com>
2019-12-27 18:24:40 -0500

    Refactored extension setup

--- grocerylist/__init__.py
+++ grocerylist/__init__.py
@@ -1,10 +1,9 @@
 from flask import Flask, request
 from itsdangerous import BadSignature
 
-from .ext import login_manager
+from .ext import init_extensions
 from .api import api_blueprint
-from .user import User
-from .util import jws, CustomSessionInterface
+from .util import CustomSessionInterface
 from .errors import register_error_handlers
 
 def create_app(config):
@@ -12,37 +11,10 @@ def create_app(config):
 
     app.config.from_mapping(config)
 
-    # Configure flask login
-    login_manager.init_app(app)
+    init_extensions(app)
 
     # disable cookie sessions
     app.session_interface = CustomSessionInterface()
-
-    @app.before_request
-    def test():
-        print(request.url)
-
-    @login_manager.request_loader
-    def load_user_from_request(request):
-        """Load user by parsing bearer token"""
-        header = request.headers.get('Authorization')
-        if header is None:
-            return None
-
-        try:
-            type_, token = header.split(' ')
-        except ValueError:
-            return None
-
-        if type_.lower() != 'bearer' or not token:
-            return None
-
-        try:
-            data = jws.loads(token)
-        except BadSignature as e:
-            return None
-
-        return User.get(int(data['id']))
 
     register_error_handlers(app)
 
--- grocerylist/ext.py
+++ grocerylist/ext.py
@@ -1,3 +1,34 @@
+"""setup flask extensions"""
+
 from flask_login import LoginManager
 
+from .util import jws
+from .user import User
+
 login_manager = LoginManager()
+
+@login_manager.request_loader
+def load_user_from_request(request):
+    """Load user by parsing bearer token"""
+    header = request.headers.get('Authorization')
+    if header is None:
+        return None
+
+    try:
+        type_, token = header.split(' ')
+    except ValueError:
+        return None
+
+    if type_.lower() != 'bearer' or not token:
+        return None
+
+    try:
+        data = jws.loads(token)
+    except BadSignature as e:
+        return None
+
+    return User.get(int(data['id']))
+
+def init_extensions(app):
+    """initialize extensions on app"""
+    login_manager.init_app(app)
