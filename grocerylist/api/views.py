7bc4259bc174516ad78bf13e8e32ae158000c688
Derek Morey <derek.o.morey@gmail.com>
2019-12-27 18:27:12 -0500

    removed unused import

--- grocerylist/api/views.py
+++ grocerylist/api/views.py
@@ -1,4 +1,4 @@
-from flask import jsonify, request, g
+from flask import request, g
 from flask.views import MethodView
 from flask_login import login_required
 from werkzeug.exceptions import NotFound
