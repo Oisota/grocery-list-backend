a2499e2b1a137e34252b1b059f651889d8a022b6
Derek Morey <derek.o.morey@gmail.com>
2020-01-06 21:53:34 -0500

    Added Flask-Migrate, created initial migration

--- grocerylist/exts/__init__.py
+++ grocerylist/exts/__init__.py
@@ -4,6 +4,7 @@ from grocerylist.exts.marshmallow import ma
 from grocerylist.exts.login import login_manager
 from grocerylist.exts.disable_cookie_session import no_cookie_sessions
 from grocerylist.exts.admin import admin
+from grocerylist.exts.migrate import migrate
 
 def init_extensions(app):
     """initialize extensions on app"""
@@ -12,3 +13,4 @@ def init_extensions(app):
     db.init_app(app)
     no_cookie_sessions.init_app(app)
     admin.init_app(app)
+    migrate.init_app(app, db)
